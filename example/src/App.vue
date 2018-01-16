4d43e2288edef8d2772a207c58890ea0ea7f292b
Bjorn <krols.bj@gmail.com>
2018-01-16 13:48:28 +0100

    Fixed example 😄

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <h1>{{title}}</h1>
+    <h1>Amazon Cognito Vuex Module Example</h1>
+    <div>
+      Authenticated: {{authenticated ? authenticated.username : 'false'}}
+      <button v-if="authenticated" @click="signOut">Sign out</button>
+    </div>
+    <hr>
+    <form @submit.prevent>
+      <input v-model="email" type="email" placeholder="email">
+      <input v-model="password" type="password" placeholder="password">
+      <button @click="authenticateUser">Authenticate</button>
+    </form>
   </div>
 </template>
 
@@ -9,8 +19,31 @@ export default {
   name: 'app',
   data() {
     return {
-      title: 'Hello, Vue!'
+      email: '',
+      password: ''
     };
+  },
+  created() {
+    this.$store.dispatch('checkAuthentication');
+  },
+  computed: {
+    authenticated() {
+      return this.$store.state.cognito.authenticated;
+    }
+  },
+  methods: {
+    authenticateUser() {
+      this.$store
+        .dispatch('authenticateUser', {
+          email: this.email,
+          password: this.password
+        })
+        .then(() => alert('authenticated'))
+        .catch(error => alert(error));
+    },
+    signOut() {
+      this.$store.dispatch('signOut');
+    }
   }
 };
 </script>
