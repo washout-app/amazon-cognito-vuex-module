a371d131d71ba5a6577c601c9e31e4af3f40cab2
Bjorn Krols <krols.bj@gmail.com>
2018-03-07 13:52:13 +0100

    Remove redundant constants

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,12 @@ const path = require('path');
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-const LIBRARY_NAME = 'index';
-const OUTPUT_FILE = `${LIBRARY_NAME}.js`;
-
 module.exports = {
   entry: [__dirname + '/src/index.js'],
   output: {
     path: __dirname + '/dist',
-    filename: OUTPUT_FILE,
-    library: LIBRARY_NAME,
+    filename: `index.js`,
+    library: 'index',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
